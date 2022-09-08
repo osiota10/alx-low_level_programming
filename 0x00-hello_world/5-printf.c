18e0e4801d208782481ec9ba530ff1798af672b2
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2022-09-08 07:18:28 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: always 0
+
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
