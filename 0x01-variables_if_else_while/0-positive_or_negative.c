db117df41ff65f8a88efee841e56c937c025c106
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-09-09 15:30:57 +0100

    Task 0 correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
 * main - Entry point
 *
@@ -9,7 +10,7 @@
 int main(void)
 {
 	int n;
-	
+		
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
