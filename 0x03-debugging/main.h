9f64af141a46772de3857b8d757f7b1854523cdb
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-09-15 16:46:06 +0100

    Task 1 correction

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H */
