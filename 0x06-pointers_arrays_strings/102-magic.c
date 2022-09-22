436b86514688441ea11c70b93aba3c5964c9b3ae
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-09-22 13:54:39 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
-* @dest: magic
+* main - print magic
 */
 
 int main(void)
@@ -13,7 +13,12 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	* @dest: magic
+	* write your line of code here...
+	* Remember:
+	* - you are not allowed to use a
+	* - you are not allowed to modify p
+	* - only one statement
+	* - you are not allowed to code anything else than this line of code
 	*/
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
