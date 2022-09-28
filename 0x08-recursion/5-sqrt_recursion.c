412a21b9e73f251d3ed518949d08e3617c301104
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2022-09-28 09:25:48 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _sqrt - function that returns the natural square root of a number
+* @x: number
+* @y: number
+* Return: sum
+*/
+
+int _sqrt(int x, int y)
+{
+	
+	int sum;
+
+	sum = x - y;
+	if (sum < 0)
+	{
+		return (-(y / 2) - 1);
+	if (sum == 0)
+		return (1);
+	else
+		return (1 + _sqrt(sum, y + 2));
+}
