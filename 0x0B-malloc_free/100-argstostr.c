b50fbac21aff5a0002f095141f8fc1cb83796203
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2022-10-04 04:00:32 -0700

    Task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* argstostr - Concatenates all arguments of the program into a string;
+* @ac: The number of arguments passed to the program
+* @av: An array of pointers to the arguments
+* Return: If ac == 0, av == NULL, or the function fails - NULL
