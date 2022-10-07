eec00e2144a699cf8f2f21571e403334871a8e99
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-10-07 02:02:09 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,50 +3,21 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
-* *string_nconcat - Function that concatenates two strings
-* @s1: string 1
-* @s2: string 2
-* @n: max bytes of s2
-* Return: Returned pointer shall point to a newly allocated space in memory
-*/
-
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ * *malloc_checked - Function that allocates memory
+ *
+ * @b: parameter
+ *
+ * Return: Return the pointer
+ */
+void *malloc_checked(unsigned int b)
 {
 	char *a;
-	unsigned int x, n1 = 0, n2 = 0, i, j;
 
-	if (s1 != NULL)
-	{
-		while (s1[n1] != 0)
-			n1++;
-	}
-	else
-		s1 = "";
-	if (s2 != NULL)
-	{
-		while (s2[n2] != 0)
-			n2++;
-	}
-	else
-		s2 = "";
-
-	if (n < n2)
-		x = n;
-	else
-		x = n2;
-	a = malloc(sizeof(char) * (n1 + x + 1));
+	a = malloc(sizeof(char) * b);
 
 	if (a == NULL)
-		return (NULL);
-	for (i = 0; i < n1; i++)
 	{
-		a[i] = s1[i];
+		exit(98);
 	}
-	for (j = 0; j < x; j++)
-	{
-		a[i + j] = s2[j];
-	}
-	a[i + j] = 0;
 
 	return (a);
-}
