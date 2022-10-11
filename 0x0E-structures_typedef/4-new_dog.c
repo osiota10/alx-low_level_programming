1824ed89bf910fbaf880071b13a49b10d28033f8
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2022-10-10 19:02:27 -0700

    Task 4 initial

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+* _strlen - returns the length of a string
+* @s: string to evaluate
+* Return: the length of the string
+*/
+
+int _strlen(char *s)
+{
+	int x;
