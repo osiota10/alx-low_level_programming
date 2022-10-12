c991473866e8bf81d6af23ec450b16496fa52ee0
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2022-10-12 10:08:19 -0700

    Task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+* op_add - function addition
+* @a: Parameter 1
+* @b: Parameter 2
+* Return: Return the result of the operation
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
