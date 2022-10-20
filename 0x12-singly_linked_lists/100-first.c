526007373c17fd2f7ba50da31db11a7abc97c6d5
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-10-20 15:14:47 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+
 /**
-* myCleanupFun - function that prints somthing before
-* the main function is executed.
-*/
+ * bmain - function executed before main
+ * Return: no return.
+ */
 
-void myCleanupFun(void) __attribute__ ((constructor));
-
-void myCleanupFun(void)
+void __attribute__((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
