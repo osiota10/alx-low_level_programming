e5a7d74dfe3dd978ec7c3136fad491d8d3173d29
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-10-25 12:46:48 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,12 +8,12 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t num = 0;
-    long int diff;
+	size_t num = 0;
+	long int diff;
 
-    while (head)
-    {
-        diff = head - head->next;
+	while (head)
+	{
+		diff = head - head->next;
         num++;
         printf("[%p] %d\n", (void *)head, head->n);
         if (diff > 0)
