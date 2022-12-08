8625efee8d841f280ce3ed20e9fb421f68e9ec1e
Osiota Samuel Obrozie <osiotaobrozie@gmail.com>
2022-12-08 10:06:23 -0800

    Task 8 correction

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-
 /**
 * delete_dnodeint_at_index - deletes the node at index of a
 * dlistint_t linked list
@@ -8,6 +7,7 @@
 * @index: index of the new node
 * Return: 1 if it succeeded, -1 if it failed
 */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *h1;
@@ -17,36 +17,23 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	h1 = *head;
 
 	if (h1 != NULL)
+
 		while (h1->prev != NULL)
 			h1 = h1->prev;
 
 	i = 0;
-
 	while (h1 != NULL)
 	{
 		if (i == index)
 		{
-			if (i == 0)
-			{
-				*head = h1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				h2->next = h1->next;
-
-				if (h1->next != NULL)
+			if (i == 0)									{											*head = h1->next;								if (*head != NULL)
+					(*head)->prev = NULL;						}
+			else										{											h2->next = h1->next;								if (h1->next != NULL)
 					h1->next->prev = h2;
 			}
-
-			free(h1);
-			return (1);
-		}
-		h2 = h1;
-		h1 = h1->next;
-		i++;
-	}
-
+													free(h1);									return (1);								}
+		h2 = h1;									h1 = h1->next;
+		i++;									}
 	return (-1);
+
 }
