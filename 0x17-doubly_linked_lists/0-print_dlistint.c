bb526ebee5502715f86981264bf49bb9a8dc64bd
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-12-08 12:18:52 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,51 +1,29 @@
 #include "lists.h"
 
 /**
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: pointer to the head of the list
  *
- *  * print_dlistint - prints all the elements of a dlistint_t list.
- *
- *   * @h: pointer to the head of the list
- *
- *     * Return: number of nodes
- *
- *      */
-
-
+ * Return: number of nodes
+ */
 
 size_t print_dlistint(const dlistint_t *h)
-
 {
 
+	int counter = 0;
 
+	if (h == NULL)
+		return (counter);
 
-		int counter = 0;
+	while (h->prev != NULL)
+		h = h->prev;
 
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		counter++;
+	}
 
-
-			if (h == NULL)
-
-						return (counter);
-
-
-
-				while (h->prev != NULL)
-
-							h = h->prev;
-
-
-
-					while (h != NULL)
-
-							{
-
-										printf("%d\n", h->n);
-
-												h = h->next;
-
-														counter++;
-
-															}
-				return (counter);
+	return (counter);
 }
-:set tabstop=4
-		
