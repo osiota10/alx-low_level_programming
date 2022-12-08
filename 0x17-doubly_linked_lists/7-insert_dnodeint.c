92995676a5488dcc4d2afbe8a3b96157e7ffa896
Osiota Samuel Obrozie <73504914+osiota10@users.noreply.github.com>
2022-12-08 19:29:24 +0100

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,46 +1,47 @@
 #include "lists.h"
-
 /**
- * insert_dnodeint_at_index - insert node at specific index
- * @h: head of linked list
- * @idx: index of new node
- * @n: new node value
- * Return: inserted node
+ * insert_dnodeint_at_index - insert node at a position
+ * @h: The head of list
+ * @idx: The index, starting at 0
+ * @n: The value of new node
+ * Return: The address of the new node, or NULL if it failed
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *current;
-	dlistint_t *new;
+	dlistint_t *actnode = *h;
+	dlistint_t *nnode;
+	unsigned int con;
 
-	if (h == NULL)
-		return (0);
-
-	current = *h;
-
-	while (idx != 0)
-	{
-		current = current->next;
-		idx--;
-		if (current == NULL)
-			return (NULL);
-	}
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-	{
-		free(new);
+	if (!h)
 		return (NULL);
+	if (*h == NULL && idx != 0)
+		return (NULL);
+	nnode = malloc(sizeof(dlistint_t));
+	if (nnode == NULL)
+		return (NULL);
+	nnode->n = n;
+	if (idx == 0)
+	{
+		if (*h)
+			actnode->prev = nnode;
+		*h = nnode;
+		nnode->prev = NULL;
+		nnode->next = actnode;
+		return (nnode);
 	}
-
-	new->n = n;
-	new->next = current;
-	new->prev = current->prev;
-	if (current->prev != NULL)
-		current->prev->next = new;
-
-	/*TODO: Handle special case when idx is 0 and last index*/
-
-	return (current);
+	for (con = 1; con < idx; con++)
+	{
+		actnode = actnode->next;
+		if (actnode == NULL)
+		{
+			free(nnode);
+			return (NULL);
+		}
+	}
+	nnode->prev = actnode;
+	nnode->next = actnode->next;
+	if (actnode->next)
+		actnode->next->prev = nnode;
+	actnode->next = nnode;
+	return (nnode);
 }
